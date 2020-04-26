b2c18b4fe52b33b8f9b957d00412bdd2c363c613
Bolshevik35 <hongquan03051997@gmail.com>
2020-04-25 18:13:02 -0700

    add particle-js background

--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particleVariables = {
+	    "particles": {
+	        "number": {
+	            "value": 150
+	        },
+	        "size": {
+	            "value": 3
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    }
+	};
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+      	    params={particleVariables} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition /> */}
     </div>
