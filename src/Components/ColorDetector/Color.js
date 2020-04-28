4deda4792633b5913952c221277e85ce99818fea
Bolshevik35 <hongquan03051997@gmail.com>
2020-04-28 16:02:19 -0700

    fix bugs in color

--- src/Components/ColorDetector/Color.js
+++ src/Components/ColorDetector/Color.js
@@ -5,11 +5,25 @@ import './Color.css';
 function Color(props){
 	let temp = props.percentage * 100;
 	temp = temp.toFixed(2);
-	return (
-		<div className='tc br3 ma2 pa1 shadow-4 bg-near-white bw2 color-list' style={{'color': props.hex}}>
-			<p>{props.name} {props.hex} {temp}%</p>
-		</div> 
-	);
+	let colorHex = props.hex;
+	if(colorHex == '#000000'){
+		return (
+			<div className='tc br3 ma2 pa2 shadow-4 bg-near-white bw2 color-list' style={{'background-color': props.hex , 'color': '#ffffff'}}>
+				<div className='items'>{props.name}</div>
+				<div className='items'>{props.hex}</div>
+				<div className='items'>{temp}%</div>
+			</div> 
+		);
+	}
+	else{
+		return (
+			<div className='tc br3 ma2 pa2 shadow-4 bg-near-white bw2 color-list' style={{'background-color': colorHex}}>
+				<div className='items'>{props.name}</div>
+				<div className='items'>{props.hex}</div>
+				<div className='items'>{temp}%</div>
+			</div> 
+		);
+	}
 }
 
 export default Color;
